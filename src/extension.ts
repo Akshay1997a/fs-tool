7d9512e0d9a6cef6ab4c6212b7777946124949aa
= <=>
2023-05-24 10:58:33 +0530

    Refactor code and added selection replace feature

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,40 @@
 import * as vscode from 'vscode';
+import * as path from 'path'
 
 export function activate(context: vscode.ExtensionContext) {
+	console.log('extension "filer" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "filer" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerTextEditorCommand('filer.openBrowse', async (textEditor, edit) => {
+	let disposable = vscode.commands.registerTextEditorCommand('filer.openBrowse', async (textEditor) => {
 		try {
-			if (vscode.workspace.workspaceFolders?.length) {
-				const currentWorkpacePath = vscode.workspace.workspaceFolders[0].uri
-				let fileUrls = await vscode.window.showOpenDialog({
-					title: "Select file",
-					canSelectMany: false,
-					defaultUri: currentWorkpacePath
-				})
-				if(fileUrls === null || fileUrls === undefined) throw new Error("Failed to get file uri!")
-				console.log(fileUrls)
-				fileUrls.forEach((fileUrl) => {
-					textEditor.edit((editBuilder) => {
-						editBuilder.replace(textEditor.selection.active, fileUrl.path)
-					})
-				})
-			} else {
-				let message = "YOUR-EXTENSION: Working folder not found, open a folder an try again" ;
-				vscode.window.showErrorMessage(message);
+			let folderUri = null;
+
+			let { activeTextEditor } = vscode.window
+			if (activeTextEditor?.document.fileName) {
+				const currentlyOpenTabfilePath = activeTextEditor.document.fileName
+				folderUri = vscode.Uri.file(path.dirname(currentlyOpenTabfilePath))
 			}
-		} catch (error) {
+
+			if(folderUri === null) {
+				let message = "YOUR-EXTENSION: Working folder not found, open a folder an try again";
+				throw new Error(message)
+			}
+
+			const dialogOptions : vscode.OpenDialogOptions = {
+				title: "Select file",
+				canSelectMany: false,
+				defaultUri: folderUri,
+			}
+			let fileUrls = await vscode.window.showOpenDialog(dialogOptions)
+			if (fileUrls === null || fileUrls === undefined) throw new Error("Failed to get file uri!")
+			fileUrls.forEach((fileUrl) => {
+				textEditor.edit((editBuilder) => {
+					editBuilder.delete(textEditor.selection)
+					editBuilder.insert(textEditor.selection.active, fileUrl.path);
+				})
+			})
+		} catch (error : any) {
 			console.log(error)
+			vscode.window.showErrorMessage(error);
 		}
 	});
 
